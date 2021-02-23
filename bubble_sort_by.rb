620120b1be7c0d76a2b2ea9cdc57d7a4b7864a1b
brytebee <brytebee@gmail.com>
2021-02-23 08:39:58 +0100

    reposition comments

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,10 +1,11 @@
 def bubble_sort_by(arr)
   # array size
   arr_length = arr.size - 1
-  # keep looping for 1 less than number of elements in array
 
   # swaps
   swap = 0
+
+  # keep looping for 1 less than number of elements in array
   arr_length.times do
     (0..(arr_length - 1)).each do |i|
       if yield(arr[i], arr[i + 1]).positive?
